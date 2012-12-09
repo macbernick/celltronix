ff51e9c8eaf9197a3727e6de75e1dce64ae37877
macbernick <vince_in_wonderland@live.fr>
2012-12-09 03:55:34 +0100

    power propagation through cells

---
+++ celltronix/sim/Sim.cs
@@ -0,0 +1,26 @@
+using System;
+
+
+
+namespace celltronix {
+	public class sim {
+
+		private Circuit circuit;
+
+		public sim(Circuit circuit) {
+			this.circuit = circuit;
+		}
+
+
+		public void run() {
+
+		}
+
+
+		private void tick() {
+
+		}
+
+	}
+}
+
